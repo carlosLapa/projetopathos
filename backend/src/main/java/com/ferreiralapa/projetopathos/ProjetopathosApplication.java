f6552ac7bef80fb33f6b2a5513db11e8d3615e39
carlosLapa <carlosferreiralapa@gmail.com>
2022-04-12 15:12:50 +0100

    Backend project created

---
+++ backend/src/main/java/com/ferreiralapa/projetopathos/ProjetopathosApplication.java
@@ -0,0 +1,13 @@
+package com.ferreiralapa.projetopathos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopathosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopathosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ferreiralapa/projetopathos/ProjetopathosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreiralapa.projetopathos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetopathosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
