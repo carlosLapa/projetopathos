27251604d1280b7a1bb16ae3976feddf6eaa77b5
carlosLapa <carlosferreiralapa@gmail.com>
2022-05-05 15:08:40 +0100

    Custom ConstraintValidator

--- backend/src/main/java/com/ferreiralapa/projetopathos/dto/UserInsertDTO.java
+++ backend/src/main/java/com/ferreiralapa/projetopathos/dto/UserInsertDTO.java
@@ -1,5 +1,11 @@
 package com.ferreiralapa.projetopathos.dto;
 
+import com.ferreiralapa.projetopathos.services.validation.UserInsertValid;
+
+/*
+ * Esta anotação vai processar a verificação, na BD, relativo ao email do utilizador
+ * */
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
