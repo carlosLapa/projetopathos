968721e2c09f49610908bf097b593514ba0ffbb1
carlosLapa <carlosferreiralapa@gmail.com>
2022-05-11 12:26:01 +0100

    Resource server

---
+++ backend/src/main/java/com/ferreiralapa/projetopathos/config/ResourceServerConfig.java
@@ -0,0 +1,49 @@
+package com.ferreiralapa.projetopathos.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Autowired
+	private JwtTokenStore jwtTokenStore;
+
+	/* Endpoint(s) público(s) */
+	private static final String[] PUBLIC = { "/oauth/token" };
+
+	private static final String[] OPERATOR_OR_ADMIN = { "/anomalias/**", "/edificios/**" };
+
+	private static final String[] ADMIN = { "/users/**" };
+
+	/*
+	 * O resourceServer vai descodificar o token e analisar se o token corresponde
+	 * às variaveis definidas, secret, validade, etc e se, portanto, é válido
+	 */
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(jwtTokenStore);
+	}
+
+	/*
+	 * Configuração das rotas e respetivas permissões/restrições
+	 */
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+		.antMatchers(ADMIN).hasRole("ADMIN")
+		.anyRequest().authenticated();
+	}
+
+}
