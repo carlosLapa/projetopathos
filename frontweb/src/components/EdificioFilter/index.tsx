50bdf1b58671cf52b8ec82394902e95af148dea6
carlosLapa <carlosferreiralapa@gmail.com>
2023-01-27 14:39:32 +0000

    EdificioFilter internal behaviour

--- frontweb/src/components/EdificioFilter/index.tsx
+++ frontweb/src/components/EdificioFilter/index.tsx
@@ -1,7 +1,8 @@
+import { getValue } from '@testing-library/user-event/dist/utils';
 import { ReactComponent as SearchIcon } from 'assets/images/search-icon.svg';
 import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import Select from 'react-select';
+import Select, { SingleValue } from 'react-select';
 import { Anomalia } from 'types/anomalia';
 import { requestBackend } from 'util/requests';
 
@@ -9,16 +10,26 @@ import './styles.css';
 
 type EdificioFilterData = {
   name: string;
-  anomalia: Anomalia;
+  anomalia: Anomalia | null;
 };
 
 const EdificioFilter = () => {
   const [selectAnomalias, setSelectAnomalias] = useState<Anomalia[]>([]);
 
-  const { register, handleSubmit, control } = useForm<EdificioFilterData>();
+  const { register, setValue, getValues, handleSubmit, control } =
+    useForm<EdificioFilterData>();
 
   const onSubmit = (formData: EdificioFilterData) => {
-    console.log('ENVIOU', formData);
+    console.log('ENVIADO', formData);
+  };
+
+  /**
+   * Função, que dispara ao clicar no botão "Limpar filtro",
+   * para "settar" (setValue) o valor do nome como string vazia e as anomalias como nulas
+   */
+  const handleFormClear = () => {
+    setValue('name', '');
+    setValue('anomalia', null);
   };
 
   useEffect(() => {
@@ -26,6 +37,23 @@ const EdificioFilter = () => {
       setSelectAnomalias(response.data.content);
     });
   }, []);
+
+  /**
+   * Ao limpar o filtro da Anomalia, vamos enviar tb o formulário.
+   * Para isso temos que passar o valor da mesma, sempre que mudar no select,
+   * assim passamos o argumento com o value da Anomalia. Desto modo, na função o tipo é Anomalia.
+   * Depois, fazemos um setValue para settar o value e ainda acrescentar os dados do formulário para ser enviado.
+   */
+  const handleChangeAnomalia = (value: Anomalia) => {
+    setValue('anomalia', value);
+
+    const obj: EdificioFilterData = {
+      name: getValues('name'),
+      anomalia: getValues('anomalia'),
+    };
+
+    console.log('ENVIADO', obj);
+  };
 
   return (
     <div className="base-card edificio-filter-container">
@@ -54,13 +82,17 @@ const EdificioFilter = () => {
                   isClearable
                   placeholder="Anomalia"
                   classNamePrefix="edificio-filter-select"
+                  onChange={(value) => handleChangeAnomalia(value as Anomalia)}
                   getOptionLabel={(anomalia: Anomalia) => anomalia.tipologia}
                   getOptionValue={(anomalia: Anomalia) => String(anomalia.id)}
                 />
               )}
             />
           </div>
-          <button className="btn btn-outline-secondary btn-edificio-filter-clear">
+          <button
+            onClick={handleFormClear}
+            className="btn btn-outline-secondary btn-edificio-filter-clear"
+          >
             LIMPAR <span className="btn-edificio-filter-word">FILTRO</span>
           </button>
         </div>
